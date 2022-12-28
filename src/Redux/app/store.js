f066df1155051eeb79e4217396e59c028a18fa74
Abir Hasan <cse.170201063@gmail.com>
2022-12-29 05:36:27 +0600

    serializableCheck problem solved

--- src/Redux/app/store.js
+++ src/Redux/app/store.js
@@ -6,6 +6,10 @@ const store = configureStore({
   reducer: {
     userR: userSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
