1ce4d14f55facf5ffdd2db0ce8cde3f8e2a9e7ac
Abir Hasan <cse.170201063@gmail.com>
2022-12-29 07:03:02 +0600

    structure done

--- src/pages/WriteDiary/WriteDiary.js
+++ src/pages/WriteDiary/WriteDiary.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import bg from '../../assets/images/background2.jpg'
 const WriteDiary = () => {
     return (
-        <div>
-            
+      <div>
+        <div className="relative">
+          <div>
+            <img className="h-screen w-full" src={bg} alt="" />
+          </div>
+          <div className="centered">
+            <h3 className="lg:block hidden text-5xl font-bold text-center text-primary fontPoppins">
+              WELCOME TO{" "}
+            </h3>
+            <h3 className="text-red-400 text-7xl font-bold font-Passions text-center">
+              Day Diary
+            </h3>
+            <p className="text-white">ds</p>
+           
+          </div>
         </div>
+      </div>
     );
 };
 
