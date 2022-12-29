1e447005f8ab70f9fdfcb2c0225c89f7d8d02109
Abir Hasan <cse.170201063@gmail.com>
2022-12-29 23:42:47 +0600

    my diary edit and delete button created

--- src/pages/MyDiary/MyDiary.js
+++ src/pages/MyDiary/MyDiary.js
@@ -4,6 +4,7 @@ import bg from "../../assets/images/background3.jpg";
 import { fetchStories } from "../../Redux/features/storySlice";
 import loading from "../../assets/images/circle-loading-lines.gif";
 import { PhotoProvider, PhotoView } from "react-photo-view";
+import dot from '../../assets/images/loading-dot.gif'
 const MyDiary = () => {
   const { isLoading, stories, error } = useSelector((state) => state.storiesR);
   const dispatch = useDispatch();
@@ -26,14 +27,16 @@ const MyDiary = () => {
             )}
 
             {error && <h4>{error}</h4>}
-            {stories.length===0 ? (
+            {stories.length === 0 ? (
               <div>
-                <p className="text-3xl font-extrabold text-center mt-5 fontPoppins">No Story Found..</p>
+                <p className="text-3xl font-extrabold text-center mt-5 fontPoppins">
+                  No Story Found..
+                </p>
               </div>
             ) : (
               <div>
                 {stories.map((story) => (
-                  <div className=" lg:mx-40 bg-base-100 bg-opacity-20 lg:p-10 mb-3">
+                  <div className=" lg:mx-40 bg-base-100 bg-opacity-20 lg:p-10 py-5 lg:pb-5 lg:py-0 mb-3">
                     <p className="text-3xl font-Passions text-center mb-3">
                       {story.post_date}
                     </p>
@@ -49,10 +52,30 @@ const MyDiary = () => {
                           </PhotoView>
                         </PhotoProvider>
                       </div>
-                      <div className="mx-5">
-                        <h3 className="text-3xl font-extrabold fontBebas tracking-wide ">
-                          {story.title}
-                        </h3>
+                      <div className="lg:ml-10 mt-3 lg:mt-0 w-full">
+                        <div className="flex justify-between items-center">
+                          <h3 className="text-3xl font-extrabold fontBebas tracking-wide ">
+                            {story.title}
+                          </h3>
+                          <div>
+                            <div className="dropdown dropdown-end">
+                              <label tabIndex={0} className="hover:cursor-pointer">
+                                <img className="w-10" src={dot} alt="" />
+                              </label>
+                              <ul
+                                tabIndex={0}
+                                className="dropdown-content menu p-2 shadow bg-base-100 bg-opacity-10 rounded-box w-24"
+                              >
+                                <li>
+                                  <a>Edit</a>
+                                </li>
+                                <li>
+                                  <a>Delete</a>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
                         <p>{story.writer_story}</p>
                       </div>
                     </div>
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 bg-opacity-20 rounded-box w-52 text-white"
             >
               {menuOptions}
             </ul>
